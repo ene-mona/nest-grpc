3d7f5abfade4e9618969eec24e456348ff3aa7f0
ene-mona <babaabel09@gmail.com>
2025-02-27 13:32:41 +0100

    fixed port

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,19 @@ import { join } from 'path';
 
 async function bootstrap() {
  
-  const PORT = process.env.PORT ?? 3000;
+  const REST_PORT = process.env.PORT ?? 3000; // Render exposes only one port (must be REST)
+  const GRPC_PORT = process.env.GRPC_PORT ?? 50051;
   const app = await NestFactory.create(AppModule);
   app.enableCors(); 
-  await app.listen(PORT);
-  console.log(`✅ REST API is running on http://localhost:${PORT}...`);
+  await app.listen(REST_PORT);
+  console.log(`✅ REST API is running on http://localhost:${REST_PORT}...`);
 
   const grpcApp = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.GRPC,
     options: {
       package: 'todo',
       protoPath: join(__dirname, '../../proto/todo.proto'),
-      url: `localhost:${PORT}`,
+      url: `localhost:${GRPC_PORT}`,
     },
   });
 
